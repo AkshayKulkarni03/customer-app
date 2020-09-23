1b3360df0e03f2a84bad89468df010933b58c1b0
Akshay Kulkarni <akshay.kulkarni8112@gmail.com>
2020-09-23 22:14:19 +0200

    Updated login component unit test with all scenarios

--- src/app/component/login/login.component.spec.ts
+++ src/app/component/login/login.component.spec.ts
@@ -3,7 +3,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, ComponentFixtureAutoDetect, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, By } from '@angular/platform-browser';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { AuthService } from './../../shared/services/auth/auth.service';
 import { TokenStorageService } from './../../shared/services/token/token-storage.service';
 import { LoginComponent } from './login.component';
@@ -11,6 +11,7 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  const { location } = window;
   let tokenSorage: TokenStorageService;
   let authService: AuthService;
 
@@ -40,6 +41,10 @@ describe('LoginComponent', () => {
     authService = TestBed.inject(AuthService);
   });
 
+  afterAll(() => {
+    window.location = location;
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -58,9 +63,61 @@ describe('LoginComponent', () => {
     flush();
 
     expect(component.isLoggedIn).toEqual(true);
+    expect(tokenSorage.getToken).toHaveBeenCalled();
+    expect(tokenSorage.getUser).toHaveBeenCalled();
+    expect(component.roles).toEqual(['ROLE_USER']);
   }));
 
+  it('should fail to login on user name password entry', () => {
+    component.isLoggedIn = false;
+    component.isLoginFailed = true;
+    component.roles = [];
+
+
+    fixture.detectChanges();
+
+    const formNativeElement = fixture.debugElement.query(By.css('form'));
+    expect(formNativeElement).toBeDefined();
+
+    const userNameField = fixture.debugElement.query(By.css('input[type=text]'));
+    expect(userNameField).toBeDefined();
+    const userNameNativeElement = userNameField.nativeElement;
+    expect(userNameNativeElement).toBeDefined();
+
+    const paswordField = fixture.debugElement.query(By.css('input[type=password]'));
+    expect(paswordField).toBeDefined();
+    const passWordNativeElement = paswordField.nativeElement;
+    expect(passWordNativeElement).toBeDefined();
+
+    fixture.detectChanges();
+
+    userNameNativeElement.value = 'test 2';
+    userNameNativeElement.dispatchEvent(newEvent('input'));
+
+    fixture.detectChanges();
+
+    passWordNativeElement.value = 'test2@password';
+    passWordNativeElement.dispatchEvent(newEvent('input'));
+
+    spyOn(component, 'onSubmit').and.callThrough();
+    spyOn(authService, 'login').and.callThrough().and.returnValue(throwError(new ErrorEvent('Error in login', { error: { message: 'error 500' } })));
+
+
+    fixture.detectChanges();
+
+    formNativeElement.triggerEventHandler('submit', null);
+
+    expect(component.onSubmit).toHaveBeenCalled();
+    expect(authService.login).toHaveBeenCalledWith({ username: 'test 2', password: 'test2@password' });
+    expect(component.isLoginFailed).toEqual(true);
+    expect(component.errorMessage).toEqual('error 500');
+
+  });
+
   it('should submit login request', () => {
+    delete window.location;
+    window.location = { ...window.location, reload: () => jest.fn() };
+
     const userNameField = fixture.debugElement.query(By.css('input[type=text]'));
     expect(userNameField).toBeDefined();
     const userNameNativeElement = userNameField.nativeElement;
@@ -87,11 +144,14 @@ describe('LoginComponent', () => {
       userName: 'test 1',
       roles: ['ROLE_USER']
     };
+
     spyOn(component, 'onSubmit').and.callThrough();
     spyOn(authService, 'login').and.callThrough().and.returnValue(of(data));
     spyOn(tokenSorage, 'saveToken').and.callThrough();
     spyOn(tokenSorage, 'saveUser').and.callThrough();
     spyOn(tokenSorage, 'getUser').and.callThrough().and.returnValue(data);
+    spyOn(window.location, 'reload').and.callThrough();
+
     fixture.detectChanges();
 
     formNativeElement.triggerEventHandler('submit', null);
@@ -99,9 +159,14 @@ describe('LoginComponent', () => {
     expect(component.onSubmit).toHaveBeenCalled();
     expect(authService.login).toHaveBeenCalledWith({ username: 'test 1', password: 'test@password' });
 
+    expect(window.location.reload).toHaveBeenCalled();
+
   });
+
 });
-function newEvent(eventName: string) {
+
+
+function newEvent(eventName: string): Event {
   const event = document.createEvent('CustomEvent');
   event.initCustomEvent(eventName, false, false, null);
   return event;
