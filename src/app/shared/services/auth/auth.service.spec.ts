ea270ffa056347982cdb609c6fc4e71e309f6cca
Akshay Kulkarni <akshay.kulkarni8112@gmail.com>
2020-09-23 22:53:15 +0200

    Added test case for auth service

--- src/app/shared/services/auth/auth.service.spec.ts
+++ src/app/shared/services/auth/auth.service.spec.ts
@@ -1,16 +1,105 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
+
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
+  const backenURL = 'http://localhost:8080/api/auth/';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should login with post service call', (done) => {
+    const loginInput = { userName: 'test1', password: 'test password' };
+    const userData = {
+      accessToken: 'test access token',
+      userName: 'test 1',
+      roles: ['ROLE_USER']
+    };
+
+    service.login(loginInput).subscribe(user => {
+      expect(user.accessToken).toEqual('test access token');
+      expect(user.roles).toEqual(['ROLE_USER']);
+      done();
+    });
+
+    const req = httpMock.expectOne(`${backenURL}signin`);
+
+    expect(req.request.method).toEqual('POST');
+    expect(req.cancelled).toEqual(false);
+    expect(req.request.headers.getAll('Content-Type')).toEqual(['application/json']);
+
+    req.flush(userData);
+  });
+
+  it('should fail to login for POST service call', () => {
+    const loginInput = { userName: 'test1', password: 'test password' };
+
+    service.login(loginInput).subscribe(
+      () => fail('should throw error for service call'),
+      (error: HttpErrorResponse) => expect(error.status).toEqual(500)
+    );
+
+    const req = httpMock.expectOne(`${backenURL}signin`);
+
+    expect(req.request.method).toEqual('POST');
+    expect(req.cancelled).toEqual(false);
+    expect(req.request.headers.getAll('Content-Type')).toEqual(['application/json']);
+
+    req.flush('error 500 in service call', { status: 500, statusText: 'INTERNAL SERVER ERROR' });
+  });
+
+  it('should regiser new user with post service call', (done) => {
+    const loginInput = { userName: 'test1', email: 'test@email.com', password: 'test@password' };
+    const userData = {
+      userName: 'test1',
+      roles: ['ROLE_USER']
+    };
+
+    service.register(loginInput).subscribe(user => {
+      expect(user.userName).toEqual('test1');
+      expect(user.roles).toEqual(['ROLE_USER']);
+      done();
+    });
+
+    const req = httpMock.expectOne(`${backenURL}signup`);
+
+    expect(req.request.method).toEqual('POST');
+    expect(req.cancelled).toEqual(false);
+    expect(req.request.headers.getAll('Content-Type')).toEqual(['application/json']);
+
+    req.flush(userData);
+  });
+
+  it('should fail to register new user for POST service call', () => {
+    const loginInput = { userName: 'test1', email: 'test email.com', password: 'test@password' };
+
+    service.login(loginInput).subscribe(
+      () => fail('should throw error for service call'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(400);
+        expect(error.type).toEqual('Error 400');
+      });
+
+    const req = httpMock.expectOne(`${backenURL}signin`);
+
+    expect(req.request.method).toEqual('POST');
+    expect(req.cancelled).toEqual(false);
+    expect(req.request.headers.getAll('Content-Type')).toEqual(['application/json']);
+
+    req.error(new ErrorEvent('Error 400', { message: 'email syntax is not correct' }), { status: 400, statusText: 'BAD REQUEST' });
+  });
+
 });
